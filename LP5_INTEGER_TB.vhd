31218308617c242dc7167e2b40f04b4f3ab0a326
SamuelMoo99 <85518527+SamuelMoo99@users.noreply.github.com>
2021-06-22 20:45:58 -0500

    Update LP5_INTEGER_TB.vhd

--- LP5_INTEGER_TB.vhd
+++ LP5_INTEGER_TB.vhd
@@ -1,36 +1,7 @@
---------------------------------------------------------------------------------
--- Company: 
--- Engineer:
---
--- Create Date:   22:59:45 06/20/2021
--- Design Name:   
--- Module Name:   C:/Users/samue/Desktop/6TO SEMESTRE/SISTEMAS DIGITALES/Xilinx/LP5_INTEGER/LP5_INTEGER_TB.vhd
--- Project Name:  LP5_INTEGER
--- Target Device:  
--- Tool versions:  
--- Description:   
--- 
--- VHDL Test Bench Created by ISE for module: LP5_INTEGER
--- 
--- Dependencies:
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
---
--- Notes: 
--- This testbench has been automatically generated using types std_logic and
--- std_logic_vector for the ports of the unit under test.  Xilinx recommends
--- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation 
--- simulation model.
---------------------------------------------------------------------------------
+
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
- 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---USE ieee.numeric_std.ALL;
+
  
 ENTITY LP5_INTEGER_TB IS
 END LP5_INTEGER_TB;
